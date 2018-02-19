714bfdea651dc8c599c275f217ee42ddfe6c46be
varsha bavane <varshab13bavane@gmail.com>
2018-02-19 13:21:59 +0530

    function created in html,n labels are given,calculate the  temprature into celciusto fahreneit n fahrenit to celcius using formula in if conditn

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  temp = {
+    celsius: null,
+    fahreneit: null
+  }
 
   constructor(public navCtrl: NavController) {
 
+
   }
+
+  tempConvertFahreneit() {
+    if (this.temp.celsius != null) {
+      this.temp.fahreneit = ((this.temp.celsius) * 9 / 5) + 32 + 'F'
+
+    } if (this.temp.celsius === '') {
+      this.temp.fahreneit = ''
+    }
+  }
+  tempConvertCelcius() {
+    if (this.temp.fahreneit != null) {
+      this.temp.celsius = ((this.temp.fahreneit) - 32) * 5 / 9 + 'C'
+    } if (this.temp.fahreneit === '') {
+      this.temp.fahreneit = ''
+    }
+  }
+
 
 }
